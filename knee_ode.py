d1c1edf5dcc738df817d217fd86add685220a1fb
jgabes <jamescgabriel@gmail.com>
2016-04-27 15:42:01 -0400

    made PID work

--- knee_ode.py
+++ knee_ode.py
@@ -13,16 +13,16 @@ class some_class(object):
         self._stiff = BOWDEN_STIFFNESS
         self._force = 0.
         self._desired_velocity=0.
-        self._desired_force=0.
-        self._kp=5.
-        self._kd=.1
+        self._desired_force=1.
+        self._kp=0.000008
+        self._kd=.5
 
 
     def pcontrol(self):
-        self._desired_velocity=self.kp*(self._desired_force-self._desired_force)
+        self._desired_velocity=self._kp*(self._desired_force-self._force)
 
     def pdcontrol(self,vel):
-        self._desired_velocity=self.kp*(self._desired_force-self._desired_force)+self._kd*vel
+        self._desired_velocity=self._kp*(self._desired_force-self._force)-self._kd*vel
 
     @property
     def stiff(self):
@@ -80,7 +80,7 @@ def ode(state, t, useful_class):
     """
     dstate_dt = np.zeros(STATE_SHAPE)
     a = useful_class.stuff
-    useful_class.pcontrol()
+    useful_class.pdcontrol(state[1])
     des_vel=useful_class.desired_velocity
     dstate_dt[0] = state[1]  # dx/dt = xdot
     dstate_dt[1] =des_vel #
@@ -89,14 +89,14 @@ def ode(state, t, useful_class):
 
 if __name__ == "__main__":
     #State: phi, f
-    initial_state = [0.,  1.]
+    initial_state = [0.,  0.0]
     state = np.array(initial_state)
     t = 0.
     dt = 0.01
     my_thing = some_class()
     time_history = [0.]
     force_history=[0.]
-    corce_history=[my_thing.force]
+    force_history=[my_thing.force]
     state_history = [initial_state]
 
     while t < 10:
@@ -113,8 +113,9 @@ if __name__ == "__main__":
     force_history=np.array(force_history)
     x_line, = plt.plot(time_history, state_history[:, 0], 'b-', linewidth=4.0, label="X")
     xdot_line, = plt.plot(time_history, state_history[:, 1], 'c-', linewidth=4.0, label="X_dot")
+    f_line, = plt.plot(time_history, force_history, 'g-', linewidth=4.0, label="f")
    # plt.legend(handles=(x_line, xdot_line))
     plt.show()
 
-    f_line, = plt.plot(time_history, force_history[:, 0], 'g-', linewidth=4.0, label="Force")
-    plt.show()
+#    f_line, = plt.plot(time_history, force_history[:, 0], 'g-', linewidth=4.0, label="Force")
+ #   plt.show()
